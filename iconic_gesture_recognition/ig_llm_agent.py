ca5d61ee62c5470eb8c17be4c930fd8642ca1b2f
emiliesdgh <emilie.grandjean@epfl.ch>
2026-05-07 07:50:44 +0200

    missed return arguments

--- iconic_gesture_recognition/ig_llm_agent.py
+++ iconic_gesture_recognition/ig_llm_agent.py
@@ -146,16 +146,19 @@ class LLMInferenceAgent:
                 
                 print(f"\n[NEW INTENT DECODED]: {self.current_intent}")
                 print(f"[REASONING]: {self.current_reasoning}\n")
-                # return prediction_json
+                return prediction_json
+            
             except json.JSONDecodeError:
                 print(f"❌ JSON Parse Error. Raw Output: {response_text}")
                 # return {"intent": "UNKNOWN"}
                 self.current_intent = "UNKNOWN"
+                return {"intent": "UNKNOWN", "reasoning": "Failed to parse LLM response."}
 
         except Exception as e:
             print(f"❌ Ollama Error: {e}")
             # return {"intent": "UNKNOWN"}
             self.current_intent = "UNKNOWN"
+            return {"intent": "UNKNOWN", "reasoning": f"Ollama Error: {e}"}
         finally:
             self.is_inferencing = False
         
