d16e5f1a6768eff69e604b93028cac7ffba908ac
emiliesdgh <emilie.grandjean@epfl.ch>
2026-05-13 15:09:04 +0900

    not better prompt --> 71%

--- iconic_gesture_recognition/ig_evaluator.py
+++ iconic_gesture_recognition/ig_evaluator.py
@@ -49,6 +49,6 @@ def evaluate_llm():
     
 
 if __name__ == "__main__":
-    logger = setup_logger("gesture_log_evaluator_goodDataset.txt")
+    logger = setup_logger("gesture_log_evaluator_goodDataset_otherPrompt.txt")
 
     evaluate_llm()
