6da4c6243bf01e654c24a7bc54670f4d2661d988
emiliesdgh <emilie.grandjean@epfl.ch>
2026-05-14 13:05:40 +0200

    added hard coded some common sense hand poses to help the llm -- seems to be helping !!

--- iconic_gesture_recognition/ig_inference.py
+++ iconic_gesture_recognition/ig_inference.py
@@ -9,30 +9,45 @@ def get_symbolic_string_2(global_vars, finger_flexion_state, finger_contact_stat
     # 1. Group fingers by state
     extended = [name.capitalize() for name, val in zip(global_vars.FINGERS["name"], finger_flexion_state) if val == 1]
     folded = [name.capitalize() for name, val in zip(global_vars.FINGERS["name"], finger_flexion_state) if val == -1]
+    hand_pose = "Unknown"
     
     if extended and folded:
         if len(extended)>=2 and len(folded)==1:
             flexion_desc = f"The {', '.join(extended)} fingers are straight, while the {', '.join(folded)} finger is bent."
         elif len(extended)==1 and len(folded)>=2:
             flexion_desc = f"The {', '.join(extended)} finger is straight, while the {', '.join(folded)} fingers are bent."
+            # # if the only straight finger is the index
+            # if extended[0] == "Index":
+            #     hand_pose = "Pointing"
+
         elif len(extended)>=2 and len(folded)>=2:
             flexion_desc = f"The {', '.join(extended)} fingers are straight, while the {', '.join(folded)} fingers are bent."
+            if len(extended) == 2 and "Thumb" in extended and "Index" in extended:
+                hand_pose = "Pointing"
         else:
             flexion_desc = f"The {', '.join(extended)} finger is straight, while the {', '.join(folded)} finger is bent."
         # check if do a condition for several fingers for the conjugaison
     elif extended:
         if len(extended) == 5:
             flexion_desc = "All fingers are straight."
+            hand_pose = "Open Palm"
         else: 
             flexion_desc = f"The {', '.join(extended)} fingers are straight, while the other fingers are relaxed."
     elif folded:
         if len(folded) == 5:
             flexion_desc = "All fingers are bent."
+            hand_pose = "Fist"
         else:
             flexion_desc = f"The {', '.join(folded)} fingers are bent, while the other fingers are relaxed."
     # should we interprete relaxed as straight ? so that only when the fingers are bent they are bent and otherwise taken into account as if straight for the intent prediction ?
     else:
         flexion_desc = "All fingers are in a relaxed, natural state."
+
+    # if the only straight finger is the index
+    if len(extended) == 1 and "Index" in extended:
+        hand_pose = "Pointing"
+    elif len(extended)==2 and "Thumb" in extended and "Index" in extended:
+        hand_pose = "Pointing"
 
     # 2. Group finger contacts
     in_contact = [name.capitalize() for name, val in zip(global_vars.FINGERS["name"][1:], finger_contact_state) if val == 1]
@@ -51,6 +66,13 @@ def get_symbolic_string_2(global_vars, finger_flexion_state, finger_contact_stat
     else:
         motion_desc = f"The hand is moving with a {spatial_motion} motion."
 
+    if hand_pose != "Unknown":
+        hand_state = f"The hand is in a {hand_pose} pose."
+    else:
+        hand_state = f"{flexion_desc}"
+
+    # print(f"DEBUG: hand_pose={hand_pose}, flexion_desc={flexion_desc}")
+
     # 3. Format the final bulleted prompt
     symbolic_str = (
         f"--- SENSOR CONFIDENCE ---\n"
@@ -65,7 +87,7 @@ def get_symbolic_string_2(global_vars, finger_flexion_state, finger_contact_stat
 
         # "Here is the current state of the user's hand:\n"
         "--- HAND STATE ---\n"
-        f"- {flexion_desc}\n"
+        f"- {hand_state}\n"
         f"- {contact_desc}\n"
         f"- The palm orientation is {hand_orientation}.\n"
         f"- The hand is positioned at {hand_position} relative to the center of the view.\n\n"
